d31c73c79e44d98ee5e994bc3716c60199001aaa
virgilwi <virgilwi@student.21-school.ru>
2024-05-24 23:33:22 +0300

    create model ShoppingItem

---
+++ src/main/java/model/ShoppingItem.java
@@ -0,0 +1,42 @@
+package model;
+
+public class ShoppingItem {
+    private Long id;
+    private String name;
+    private int quantity;
+
+    public ShoppingItem(Long id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingItem{" + "id=" + id + ", name=" + name + '\'' + ", quantity=" + quantity + '}';
+    }
+}
